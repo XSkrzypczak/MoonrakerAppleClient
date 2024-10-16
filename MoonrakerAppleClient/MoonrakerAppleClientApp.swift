c993b146a586fec0637791ae1ff8aff9a635bfbd
XSkrzypczak <skrzypczakmiki@gmail.com>
2024-10-16 19:31:47 +0200

    Initial Commit

---
+++ MoonrakerAppleClient/MoonrakerAppleClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonrakerAppleClientApp.swift
+//  MoonrakerAppleClient
+//
+//  Created by Mikolaj Skrzypczak on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonrakerAppleClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
