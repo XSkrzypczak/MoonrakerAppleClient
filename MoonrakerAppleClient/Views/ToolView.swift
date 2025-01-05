19420ed3b4e0f2c3a0e70cc7daf735742fa384b6
XSkrzypczak <skrzypczakmiki@gmail.com>
2025-01-05 19:12:46 +0100

    add Views folder, add ToolView

--- MoonrakerAppleClient/Views/ToolView.swift
+++ MoonrakerAppleClient/Views/ToolView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 
 struct ToolView: View {
+    @EnvironmentObject var printer: Printer
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
     }
 }
 
 #Preview {
     ToolView()
+        .environmentObject(Printer(url: URL(string:"ws://192.168.88.39:7125")!))
 }
